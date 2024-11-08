1aac65598700e4d909692dd6ffdd351a95276ee8
Gaute Akre Dommarsnes <e-gaudom@iskule.no>
2024-11-08 11:13:35 +0100

    h

---
+++ Js/test2.js
@@ -0,0 +1,9 @@
+function finnAlder() {
+
+    let tall1 = Number(prompt("Hva er det første tallet?", ""));
+    let tall2 = Number(prompt("Hva er det andre tallet?", ""));
+    let navn = prompt("Hva er navnet ditt?", "");
+    let alder = (tall1 + tall2);
+
+    document.getElementById("finnAlder").innerHTML = navn + ", du er " + alder + "år gammel!";
+}
