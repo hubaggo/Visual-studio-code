9b45d957f363de88ecfdd60fabffb12b5f04acb7
Gaute Akre Dommarsnes <e-gaudom@iskule.no>
2024-11-18 13:04:26 +0100

    h

--- Js/test2.js
+++ Js/test2.js
@@ -1,9 +1,40 @@
-function finnAlder() {
+document.getElementById("play").addEventListener("click", play)
 
-    let tall1 = Number(prompt("Hva er det første tallet?", ""));
-    let tall2 = Number(prompt("Hva er det andre tallet?", ""));
-    let navn = prompt("Hva er navnet ditt?", "");
-    let alder = (tall1 + tall2);
-
-    document.getElementById("finnAlder").innerHTML = navn + ", du er " + alder + " år gammel!";
+let humanScore = 0
+let computerScore = 0
+ 
+function play() {
+    const choices = ["stein", "papir", "saks"]
+    let value = document.getElementById("choice").value
+    let robot = choices[Math.floor(Math.random() * 3)]
+    if (!choices.includes(value)) {
+        console.log("Not valid choice")
+    }
+    if (value === robot) {
+        console.log("Tie")
+        document.getElementById("result").innerText="Tie"
+    }
+    else if (value === "stein" & robot === "saks") {
+        console.log("Player won")
+        document.getElementById("result").innerText="Player won"
+        humanScore++
+        
+    }
+    else if (value === "papir" & robot === "stein") {
+        console.log("Player won")
+        document.getElementById("result").innerText="Player won"
+        humanScore++
+    }
+    else if (value === "saks" & robot === "papir") {
+        console.log("Player won")
+        document.getElementById("result").innerText="Player won"
+        humanScore++
+    }
+    else {
+        console.log("Robot won")
+        document.getElementById("result").innerText="Robot won"
+        computerScore++
+    }
+    document.getElementById("humanScore").innerText="Player score: " + humanScore
+    document.getElementById("computerScore").innerText="Robot score: " + computerScore
 }
