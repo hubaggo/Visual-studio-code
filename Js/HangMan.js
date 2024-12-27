5481104fbf9034702247facfdc5910973f15b48f
Gaute Akre Dommarsnes <e-gaudom@iskule.no>
2024-12-27 15:14:43 +0100

    h

--- Js/HangMan.js
+++ Js/HangMan.js
@@ -4,8 +4,12 @@ function man() {
     const ord = ["tilfeldig", "neshorn", "regnbyger", "tåkedotter", "ørnerede", "dagligvarer", "skogbruk", "skolegang"];
     let riktig = ord[Math.floor(Math.random() * 8)];
     let lengde = riktig.length;
-    let bokstav = String(prompt())
+    let bokstav = String(prompt("Første bokstav", ""))
+    if (bokstav === "") {
+        console.log("Skriv inn en bokstav")
+        return;
+    }
     for (let i = 0; i > lengde; i++) {
-        
+        /*Bruk av "indexOf" er lurt her, se w3schools og Knut for mer hjelp*/
     }
 }
