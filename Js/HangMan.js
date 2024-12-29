1d34a6cac20c411ae4d85fe01fdda28dc7b8c36b
Gaute Akre Dommarsnes <e-gaudom@iskule.no>
2024-12-29 15:10:22 +0100

    h

--- Js/HangMan.js
+++ Js/HangMan.js
@@ -44,9 +44,14 @@ function gjett() {
         feilbokstaver += feilliste[i] + " ";
     }
     if (vant === true) {
-        document.getElementById("ferdigknap").style.display="inline"
+        document.getElementById("ferdigknap").style.display="inline";
+        document.getElementById("resultat").innerText="Gratulerer, du vant! :)";
     }
-    //Noe som fikser den "Du vant/tapte" ti ngen jeg snakket om i html dellen//
+    else if (feil === 6) {
+        document.getElementById("ferdigknap").style.display="inline";
+        document.getElementById("resultat").innerText="Å nei! Du tapte! :(";
+    }
+    //Noe som fikser den "Du vant/tapte" tingen jeg snakket om i html dellen//
     //No som gjør at man kan tape f.eks feil blir større en et førbestemt tall//
     document.getElementById("feilplass").innerText=feilbokstaver;
     document.getElementById("gjetteplass").innerText=gjetteplass;
